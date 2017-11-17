98fa88af60c5b0e28898e2866fbbee2545b3ddd6
Simon CORNAZ <scornaz@e2r9p1.42.fr>
2017-11-17 12:11:18 +0100

    python

--- tests.py
+++ tests.py
@@ -1,2 +1,17 @@
-def tuples(n):
-    
+import sys
+
+mat = []
+for i in range(16):
+     mat.append('.')
+
+def w(a):
+    sys.stdout.write(a)
+
+def p_mat():
+    for i in range(16):
+        if (i and not(i % 4)):
+            w('\n')
+        w(mat[i])
+
+p_mat()
+
