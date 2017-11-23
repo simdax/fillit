0930733dc57e059246135409076acb03cc62a876
Simon CORNAZ <scornaz@e1r12p21.42.fr>
2017-11-23 10:11:05 +0100

    OK_algo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/22 09:48:50 by scornaz           #+#    #+#             */
-/*   Updated: 2017/11/22 19:44:28 by scornaz          ###   ########.fr       */
+/*   Updated: 2017/11/23 10:10:34 by scornaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void	clean(char *matrice, size_t places[5])
 	}		
 }
 
-int		add(char *tetromino, char *matrice, size_t pos, size_t size)
+int		add(char *tetromino, char *matrice, size_t pos, size_t size, size_t index)
 {
 	unsigned int	row;
 	size_t			puts[5];
@@ -48,14 +48,14 @@ int		add(char *tetromino, char *matrice, size_t pos, size_t size)
 	{
 		if (*tetromino == '#')
 		{
-			if (matrice[pos] == '#' ||
+			if (matrice[pos] != '.' ||
 				pos > size * size || pos / size > row)
 			{
 				clean(matrice, puts);
 				return (0);
 			}
 				puts[i++] = pos;
-				matrice[pos] = '#';
+				matrice[pos] = 'A' + index;
 		}
 		else if (*tetromino == '\n')
 		{
@@ -87,11 +87,16 @@ int		resolve(int size)
 	while (i < 19)
 	{
 		int j = 0;
-		while (j < size*size && !add(g_tetros[i], matrice, j, size))
+		int res = 0;
+		while (j < size*size && !(res = add(g_tetros[i], matrice, j, size, i)))
 			j++;
-		print(matrice, size);
+		if (!res) {
+			resolve(size + 1);
+			return (0);
+		}
 		i++;
 	}
+	print(matrice, size);
 	return (0);
 }
 
