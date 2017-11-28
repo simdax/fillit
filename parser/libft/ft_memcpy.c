e540dcbe30c8626a4ebb5793fa487b1255e3483e
Simon CORNAZ <scornaz@e1r3p7.42.fr>
2017-11-28 15:12:14 +0100

    added_parser

---
+++ parser/libft/ft_memcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nschwarz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/10 13:38:24 by nschwarz          #+#    #+#             */
+/*   Updated: 2017/11/10 14:10:39 by nschwarz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h>
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char	*s1;
+	char	*s2;
+
+	if (n == 0 || dst == src)
+		return (dst);
+	s1 = (char *)dst;
+	s2 = (char *)src;
+	while (--n)
+		*s1++ = *s2++;
+	*s1 = *s2;
+	return (dst);
+}
---
+++ parser/srcs/main.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nschwarz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/17 15:24:57 by nschwarz          #+#    #+#             */
+/*   Updated: 2017/11/28 14:20:00 by scornaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "fillit.h"
+
+#include	<stdio.h>
+
+int		ft_caller(char **secmap)
+{
+	char	*map1d;
+
+	map1d = ft_strjoin(secmap[0], secmap[1]);
+	map1d = ft_strjoin(map1d, secmap[2]);
+	map1d = ft_strjoin(map1d, secmap[3]);
+	return (ft_parse(ft_preparse(map1d)));
+}
+
+void	print(int *ret, int len)
+{
+	while (len--)
+	{
+		ft_putnbr(*ret);
+		ft_putchar(' ');
+		ret++;
+	}
+}
+int		main(int argc, char **argv)
+{
+	int		fd;
+	char	*map;
+	char	**secmap;
+	int		i;
+	int		cur;
+	int		nb_tetri;
+			
+	cur = 0;
+	i = 0;
+	if (argc != 2)
+	{
+		ft_putstr("usage: ./fillit source_file\n");
+		return (0);
+	}
+	fd = open(*++argv, O_RDONLY);
+	map = ft_read_map(fd);
+	secmap = ft_strsplit(map, '\n');
+	nb_tetri = ft_nbtetri(secmap) / 4;
+	int	*ret = (int*)malloc(sizeof(int) * (nb_tetri + 1));
+	while (secmap[i])
+	{
+		ret[cur] = ft_caller(secmap + i);
+		i += 4;
+		cur++;		
+	}
+	ret[cur] = 0;
+	close(fd);
+	print(ret, nb_tetri);
+	return (0);
+}
