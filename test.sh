d3cb990312268c078cbfe6456bffee1a349a9810
Simon CORNAZ <scornaz@e1r3p7.42.fr>
2017-11-28 13:35:21 +0100

    added_tests

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-./parser Tetriminos-generator/sample.fillit | xargs solver/solver
+
+Tetriminos-generator/tetri-gen -v -f 10
+
+./parser Tetriminos-generator/sample.fillit | xargs solver/solver > nous
+./claudio/fillit Tetriminos-generator/sample.fillit > cloclo
+diff nous cloclo
