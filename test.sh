9817e4ff5a062d124ec52b322ac524b543e195a3
Simon CORNAZ <scornaz@e1r3p7.42.fr>
2017-11-28 14:12:46 +0100

    test

--- test.sh
+++ test.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+git checkout nono2 parser
+git checkout sim fillit
+
 for i in {0..10}; do
 echo test numero $i
-Tetriminos-generator/tetri-gen -v -f 5
+Tetriminos-generator/tetri-gen -v -f 10
+cat sample.fillit
 ./parser sample.fillit | xargs ./fillit > nous
 ./claudio/fillit sample.fillit > cloclo
-cat nous
-cat cloclo
+##cat nous
+##cat cloclo
 diff nous cloclo
 done
