edf71ee685c7cead1d5359d0318008e7b1600403
n0n0man <nschwarz@student.42.fr>
2017-12-01 13:08:19 +0100

    a

--- Makefile
+++ Makefile
@@ -1,49 +1,9 @@
-SRC_PATH = .
-
-SRC_NAME =  ft_error.c ft_read_map.c main.c parser.c solver.c utils.c
-
-OBJ_PATH = .
-
-INC_PATH = .
-
-CPPFLAGS = -I./libft
-
-LDFLAGS = -Llibft
-LDLIBS = -lft
-
-NAME = fillit
-
-CC = clang
-CFLAGS = -Werror -Wall -Wextra
-
-OBJ_NAME = $(SRC_NAME:.c=.o)
-
-SRC = $(addprefix $(SRC_PATH)/,$(SRC_NAME))
-OBJ = $(addprefix $(OBJ_PATH)/,$(OBJ_NAME))
-
-.PHONY: all, clean, fclean, re
-
-all: $(NAME)
-
-$(NAME): $(OBJ)
-	make -C ./libft/
-	$(CC) $(LDFLAGS) $(LDLIBS) $^ -o $@
-
-$(OBJ_PATH)%.o: $(SRC_PATH)%.c
-	@mkdir $(OBJ_PATH) 2> /dev/null || true
-	$(CC) $(CFLAGS) $(CPPFLAGS) -o $@ -c $<
+all:
+	gcc -g main.c parser/parser.a solver/solver.a libft/libft.a -o fillit -I./libft
 
 clean:
-	make clean -C ./libft/
-	/bin/rm -fv $(OBJ)
-	@rmdir $(OBJ_PATH) 2> /dev/null || true
-
+	rm -f *o
 fclean: clean
-	make fclean -C ./libft/
-	/bin/rm -fv $(NAME)
-
-re: fclean all
-
-norme:
-	@norminette $(SRC)
-	@norminette $(INC_PATH)/*.h
+	rm -f fillit
+re:	fclean
+	make all
--- main.c
+++ main.c
@@ -6,20 +6,17 @@
 /*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 14:53:45 by scornaz           #+#    #+#             */
-/*   Updated: 2017/12/01 12:25:09 by nschwarz         ###   ########.fr       */
+/*   Updated: 2017/11/30 12:25:04 by nschwarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include "fillit.h"
+# include <stdlib.h>
+# include "libft.h"
 
-void	stupid(size_t *i, size_t *pos, size_t *size, unsigned int *row)
-{
-	*i = 0;
-	*row = *pos / *size;
-}
+int		resolve(char **argv, size_t size);
+char	**parse(char **argv);
 
-int		main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	char	**parsed;
 
@@ -29,7 +26,8 @@ int		main(int argc, char **argv)
 		return (0);
 	}
 	if (!(parsed = parse(argv)))
-		ft_putstr("error\n");
+		ft_putstr_fd("error\n", 2);
 	else
 		resolve(parsed, 3);
+	free(parsed);
 }
